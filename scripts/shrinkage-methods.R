51367ec4e4a1296180715b58307230b0333c9ee6
Kasper Rosenkrands <krosenkrands7@gmail.com>
2021-05-28 21:17:00 +0200

    progress on topic 2

--- scripts/shrinkage-methods.R
+++ scripts/shrinkage-methods.R
@@ -16,12 +16,28 @@ lambda_seq <- 10^seq(4, -2, by = -.1)
 
 # Using glmnet function to build the ridge regression in r
 ridge.fit <- glmnet(x_var, y_var, alpha = 0, lambda  = lambda_seq)
-
-tibble(as.data.frame(t(as.matrix(coef(ridge.fit)))[,-1])) %>%
+ridge.data <- tibble(as.data.frame(t(as.matrix(coef(ridge.fit)))[,-1])) %>%
   mutate(lambda = lambda_seq) %>%
-  pivot_longer(cols = !lambda) %>%
+  pivot_longer(cols = !lambda)
+
+ridge.data %>%
   ggplot(aes(x = lambda, y = value, linetype = name, color=name)) +
     geom_line() +
     scale_x_log10() +
-    labs(y="Standardized coefficients")
+    labs(y="Standardized coefficients", color = "Variable", linetype = "Variable")
 ggsave('output/ridge_coefficient.pdf', width=8,height=4)
+
+# Using glmnet function to build the lasso regression in r
+lambda_seq <- 10^seq(4, -2, by = -.1)
+lasso.fit <- glmnet(x_var, y_var, alpha = 1, lambda  = lambda_seq)
+lasso.data <- tibble(as.data.frame(t(as.matrix(coef(lasso.fit)))[,-1])) %>%
+  mutate(lambda = lambda_seq) %>%
+  pivot_longer(cols = !lambda)
+
+lasso.data %>%
+  ggplot(aes(x = lambda, y = value, color=name, linetype = name)) +
+  geom_line(data=ridge.data, aes(x=lambda,y=value,group=name),color='gray') +
+  geom_line() +
+  scale_x_log10() +
+  labs(y="Standardized coefficients", color = "Variable", linetype = "Variable")
+ggsave('output/lasso_coefficient.pdf', width=8,height=4)
