8b4d327b166bf434271fe1a87b0bcf68fdebca42
Kasper Rosenkrands <krosenkrands7@gmail.com>
2021-06-02 12:40:45 +0200

    tweak script for topic 6

--- scripts/neural-net.R
+++ scripts/neural-net.R
@@ -31,7 +31,11 @@ plot(nn)
 
 nn.rmse <- modelr::rmse(nn, test)
 lin.rmse <- modelr::rmse(lin, test)
-cat('nn rmse:', round(nn.rmse, 3), 'lm rmse:', round(lin.rmse, 3), '\n')
+cat('test nn rmse:', round(nn.rmse, 3), 'lm rmse:', round(lin.rmse, 3), '\n')
+
+nn.rmse <- modelr::rmse(nn, train)
+lin.rmse <- modelr::rmse(lin, train)
+cat('train nn rmse:', round(nn.rmse, 3), 'lm rmse:', round(lin.rmse, 3), '\n')
 
 pred.nn <- predict(nn, test)
 pred.lm <- predict(lin, test)
@@ -46,3 +50,33 @@ bind_rows(nn.test, lm.test) %>%
   labs(x='Actual values', y='Predicted values') +
   theme(legend.position = 'none')
 ggsave('output/neural_network_compare.pdf', width=8, height=4)
+
+pred.nn <- predict(nn, train)
+pred.lm <- predict(lin, train)
+nn.train <- tibble(real = train$cnt, pred = pred.nn, model = rep('Neural Network', nrow(train)))
+lm.train <- tibble(real = train$cnt, pred = pred.lm, model = rep('Linear Regression', nrow(train)))
+
+bind_rows(nn.train, lm.train) %>%
+  ggplot(aes(x = real, y = pred)) +
+  geom_abline(size = 1) +
+  geom_point(aes(color = model)) +
+  facet_wrap(~model) +
+  labs(x='Actual values', y='Predicted values') +
+  theme(legend.position = 'none')
+ggsave('output/neural_network_compare_train.pdf', width=8, height=4)
+
+nn <- neuralnet::neuralnet(
+  cnt ~ atemp + hum + windspeed,
+  data = train,
+  hidden = c(0),
+  act.fct = softplus
+)
+plot(nn)
+
+nn <- neuralnet::neuralnet(
+  cnt ~ atemp + hum + windspeed,
+  data = train,
+  hidden = c(2),
+  act.fct = softplus
+)
+plot(nn)
